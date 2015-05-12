353b13dbeb3545bf0947ba17611f8e294540b249
jesusalexander <brpoper@gmail.com>
2015-05-12 23:28:59 +0800

    upload 127 bug

--- entry/uploadToQiniu.py
+++ entry/uploadToQiniu.py
@@ -2,8 +2,8 @@ from qiniu import Auth
 from qiniu import put_data
 
 def uploadToQiniu(file,filename):
-    access_key = 'access_key'
-    secret_key = 'secret_key'
+    access_key = '33ZeBMUplEyElHGEPVyw9g6z-3pSDlOiwJBVCn9W'
+    secret_key = 'tPnroK1ks0KLTCaVTZ49B6XcTCViAP_P01i2OfRv'
     bucket_name = 'pikach'
     q = Auth(access_key, secret_key)
     token = q.upload_token(bucket_name)
