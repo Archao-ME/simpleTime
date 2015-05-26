3fdecd59dc2b4c5c58baac4b1d3455f6a4dc1aaf
jesusalexander <brpoper@gmail.com>
2015-05-27 00:00:17 +0800

    getrank week

--- getRank/views.py
+++ getRank/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render_to_response
 from django.http import Http404, HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+import json
 import get163Rank
 import datetime 
 from models import SongList
@@ -21,4 +22,19 @@ def getRank(request,ID):
         mysong.SaveWeek()
         songList = SongList.objects.filter(user_ID = id,date=today)
 
-    return render_to_response('getRank.html',{'songList':songList},context_instance = RequestContext(request))
+    return render_to_response('getRank.html',{'songList':songList,'user_ID':id},context_instance = RequestContext(request))
+
+def getRankDate(request,ID,date):
+    ISOFORMAT='%Y%m%d'
+    id = ID 
+    songList = SongList.objects.filter(user_ID = id,date=date)
+    if not songList :
+        return HttpResponse("500")
+    else:
+        songDic = {}
+        for song in songList:
+            songDic[song.song_ID] = {"name":song.song_Name,"score":song.score}
+
+        songJson = json.dumps(songDic)
+        print songJson
+        return render_to_response('getRankAPI.html',{'songList':songList},context_instance = RequestContext(request))
--- simpleTime/urls.py
+++ simpleTime/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from entry.views import entry,upload,person,article,login,logoutView,upload
 from entry.views import articleList
+from getRank.views import getRank
+from getRank.views import getRankDate
 urlpatterns = [
     # Examples:
     # url(r'^$', 'simpleTime.views.home', name='home'),
@@ -16,5 +18,6 @@ urlpatterns = [
     url('^entry/logout/$',logoutView),
     url('^entry/upload/$',upload),
     url('^entry/list/$',articleList),
-    
+    url('^getRank/(\d{1,10})',getRank),
+    url('^getRank/api/week/(\d{1,10})/(\d{8})',getRankDate),
 ]
